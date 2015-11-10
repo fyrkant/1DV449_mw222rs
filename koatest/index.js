7567895a86f78c61effb9765557151465c2d4b48
Mattias <mw222rs@student.lnu.se>
2015-11-10 23:06:12 +0100

    yield

--- koatest/index.js
+++ koatest/index.js
@@ -4,6 +4,8 @@ var route = require('koa-route');
 var parse = require('co-body');
 var koa = require('koa');
 var rp = require('request-promise');
+var cheerio = require('cheerio');
+var _ = require('lodash');
 var app = koa();
 var scraper = require('./scraper/scraper');
 
@@ -22,11 +24,43 @@ function *scrape() {
 
 	console.log(post.url);
 
+	var options = {
+		uri: post.url,
+		transform: function (body) {
+			return cheerio.load(body);
+		}
+	};
+	var data = {};
+
+	yield rp(options)
+		.then(function ($) {
+			$('a').each(function(){
+				data[$('a').index(this)] = options.uri+$(this).attr('href');
+			});
+		})
+		.then(function(){
+			console.log('log1',data);
+
+		})
+		.catch(function (err) {
+			console.log(err);
+		})
+
+	var calendarOptions = {
+		uri: data[0],
+		transform: function (body) {
+			return cheerio.load(body);
+		}
+	};
+
+	yield rp(calendarOptions)
+		.then(function($) {
+			console.log($.html());
+		})
+
 	var data = yield scraper(post.url);
 
-	await console.log(data);
-
-	this.redirect('/');
+	this.redirect('/', {title: post.url});
 }
 
 
