ceef632a9b56aca0fc3dea15eb89a8e7502f284b
Mattias <mw222rs@student.lnu.se>
2015-11-12 12:40:36 +0100

    linting, promises, love, peace, understanding

--- koatest/index.js
+++ koatest/index.js
@@ -6,6 +6,7 @@ var koa = require('koa');
 var rp = require('request-promise');
 var cheerio = require('cheerio');
 var _ = require('lodash');
+var async = require('async');
 var app = koa();
 var calendarScraper = require('./scraper/calendarScraper');
 
@@ -16,75 +17,81 @@ app.use(route.get('/', index));
 app.use(route.post('/', scrape));
 
 function *index() {
-	this.body = yield render('index', {title: 'webbskraparn'})
+    this.body = yield render('index', {title: 'webbskraparn'});
 }
 
 function *scrape() {
-	var post = yield parse(this);
+    var post = yield parse(this);
 
-	console.log(post.url);
+    console.log(post.url);
 
-	var options = {
-		uri: post.url,
-		transform: function (body) {
-			return cheerio.load(body);
-		}
-	};
-	var links = {
-		mainPage: '', 
-		persons: ''
-	};
+    var options = {
+        uri: post.url,
+        transform: function(body) {
+            return cheerio.load(body);
+        }
+    };
+    var links = {
+        mainPage: '',
+        persons: ''
+    };
 
-	var freeDays = {};
-	var theDayToMeet = '';
+    var freeDays = {};
+    var calendarLinks = [];
 
-	yield rp(options)
-		.then(function ($) {
-			var mainPageLinks = {};
-			$('a').each(function(){				
-				mainPageLinks[$('a').index(this)] = options.uri+$(this).attr('href');
-				
-			});
-			links['mainPage'] = mainPageLinks;
-		})
-		.then(function(){
-			console.log('log1', links);
+    yield rp(options).
+        then(function($) {
+            var mainPageLinks = {};
 
-		})
-		.catch(function (err) {
-			console.log(err);
-		})
+            $('a').each(function() {
+                mainPageLinks[$('a').index(this)] =
+                    options.uri + $(this).attr('href');
+            });
+            links['mainPage'] = mainPageLinks;
+        }).
+        then(function() {
+            console.log('log1', links);
 
-	var calendarOptions = {
-		uri: links.mainPage[0],
-		transform: function (body) {
-			return cheerio.load(body);
-		}
-	};
+        }).
+        catch(function(err) {
+            console.log(err);
+        });
 
-	yield rp(calendarOptions)
-		.then(function($) {
-			var personLinks = {};
-			$('a').each(function(){
-				personLinks[$('a').index(this)] = links.mainPage[0] + '/' + $(this).attr('href');
-			});
-			links['persons'] = personLinks;
-			return personLinks
-		})
-		.then((links) =>{
-			freeDays = _.map(links, calendarScraper);
-		})
-		.then(() => {
-			console.log(freeDays);
-		})
-		
+    var calendarOptions = {
+        uri: links.mainPage[0],
+        transform: function(body) {
+            return cheerio.load(body);
+        }
+    };
 
-		
-	//freeDays = _.map(links.persons, calendarScraper(link));
-	
-	console.log('logging free days: ', freeDays);
+    yield rp(calendarOptions).
+        then(function($) {
+            var personLinks = [];
 
-	this.redirect('/', {title: post.url});
+            $('a').each(function() {
+                personLinks.push(
+                    links.mainPage[0] + '/' + $(this).attr('href'));
+            });
+            links['persons'] = personLinks;
+            calendarLinks = personLinks;
+            return personLinks;
+        });
+
+    freeDays = calendarLinks.map((link) =>
+        rp(link).
+        then(function(html) {
+            return calendarScraper(cheerio.load(html));
+        }));
+
+    //freeDays = _.map(links.persons, calendarScraper(link));
+
+    freeDays = yield Promise.all(freeDays).
+        then((values) => {return values;});
+
+    console.log(freeDays);
+    //freeDays.forEach((prom) => prom.then(link => console.log(link)));
+
+    this.redirect('/', {title: post.url});
 }
 
 
--- koatest/scraper/calendarScraper.js
+++ koatest/scraper/calendarScraper.js
@@ -1,36 +1,27 @@
 var rp = require('request-promise');
 var cheerio = require('cheerio');
 
-var calendarScraper = function*(uri){
-	var freeDays = {};
-	var foundFreeDays = [];
+var calendarScraper = function($) {
+    var name = $('h2.center').text();
+    var days = [];
+    var foundFreeDays = [];
 
-	yield rp(uri)
-		.then((html) => {
-			return cheerio.load(html);
-		})
-		.then(function($){
-			var name = $('h2.center').text();
-			var days = [];
+    $('th').each(function() {
+        var day = $(this).text();
 
-			$('th').each(function(){
-				var day = $(this).text();
-				days.push(day);
-			});
-			$('td').each(function () {
-				var text = $(this).text();
-				var index = $('td').index(this);
-				if (text.toLowerCase() === 'ok') {
-					foundFreeDays.push(days[index]);
-				}
-			});
-			//freeDays[name] = foundFreeDays;
-			return foundFreeDays;
-		})
+        days.push(day);
+    });
+    $('td').each(function() {
+        var text = $(this).text();
+        var index = $('td').index(this);
 
-	return foundFreeDays;
-
-	
-}
+        if (text.toLowerCase() === 'ok') {
+            foundFreeDays.push(days[index]);
+        }
+    });
+    //freeDays[name] = foundFreeDays;
+    //console.log('calendarScraper: ',foundFreeDays);
+    return foundFreeDays;
+};
 
 module.exports = calendarScraper;
