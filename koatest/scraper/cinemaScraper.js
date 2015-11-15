011207155ac3be7ec4ed30bc080e019bae4c904f
Mattias <mw222rs@student.lnu.se>
2015-11-16 00:25:18 +0100

    added flightplan, tested deployment. all good. time to sleep

--- koatest/scraper/cinemaScraper.js
+++ koatest/scraper/cinemaScraper.js
@@ -2,29 +2,30 @@ var rp = require('request-promise');
 var cheerio = require('cheerio');
 
 var cinemaScraper = function*(url){
-	var firstPageLinks = [];
+    var firstPageLinks = [];
 
-	var data = yield rp(url)
-		.then(
-			function(htmlString) {
-				var $ = cheerio.load(htmlString);
-				return $('a').each(
-					function(i, el) {
-						firstPageLinks.push($(el).attr('href'));
-					}
-				);
-			})
-		.then(function(){
-			firstPageLinks.forEach(function(el) {
-				var test;
-			})
-		})		
-		.catch(
-			function(err){
-				console.log(err);
-			});
+    var data = yield rp(url).
+        then(
+            function(htmlString) {
+                var $ = cheerio.load(htmlString);
+                
+                return $('a').each(
+                    function(i, el) {
+                        firstPageLinks.push($(el).attr('href'));
+                    }
+                );
+            })
+        .then(function(){
+            firstPageLinks.forEach(function(el) {
+                var test;
+            })
+        })      
+        .catch(
+            function(err){
+                console.log(err);
+            });
 
-	return data;
+    return data;
 }
 
 module.exports = cinemaScraper;
